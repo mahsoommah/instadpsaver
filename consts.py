e15d8bc190c01d04becbe634f49ef4072c4406e9
Mahsoom <97832473+mahsoommah@users.noreply.github.com>
2022-02-15 15:24:13 +0530

    Update consts.py

--- consts.py
+++ consts.py
@@ -3,23 +3,24 @@ from telegram.utils.helpers import escape_markdown as es
 
 
 def welcome_msg():
-    welcome_msg = '''<b>Welcome To the Bot</b>🖐🖐
- <i>Send me anyones instagram username or profile url to get their DP</i>
- ex : <b>virat.kohli</b> , <b>thenameisyash</b> etc'''
+    welcome_photo = '''https://telegra.ph/file/1ab200205580a4444561b.jpg'''
+    welcome_msg = '''<b>💖Welcome To Instagram Dp Saver Bot💖</b>🖐🖐
+ <i>😋Send me anyones instagram username or profile url to get their DP😝</i>
+ ex : <b>mahsoom.mjm.18</b> etc'''
 
-    return welcome_msg
+    return welcome_msg,welcome_photo
 
 
 def acc_type(val):
     if(val):
-        return "🔒Private🔒"
+        return "🙊Secret🙈Account🙊"
     else:
-        return "🔓Public🔓"
+        return "😌Public😌"
 
 
 def create_caption(user):
-    caption_msg = f'''📛*Name*📛: {es(user.full_name,version=2)} \n😁*Followers*😁: {es(str(user.followers),version=2)} \n🤩*Following*🤩: {es(str(user.followees),version=2)}\
-        \n🧐*Account Type*🧐: {acc_type(user.is_private)} \n\nThank You For Using The bot 😀😀'''
+    caption_msg = f'''💖*Name*💖: {es(user.full_name,version=2)} \n💝*Followers*💝: {es(str(user.followers),version=2)} \n💞*Following*💞: {es(str(user.followees),version=2)}\
+        \n😉*Account Type*😉: {acc_type(user.is_private)} \n\n😘😍Thank You For Using Me 😘😍'''
 
     return caption_msg
 
@@ -32,11 +33,11 @@ def get_username(url):
                 data = data.split("?")
                 return data[0]
             except Exception:
-                return "incorrect format"
+                return "😢incorrect format😢"
         return data
     except Exception:
-        return "incorrect format"
+        return "😢incorrect format😢"
 
 
 ratingkey = [[InlineKeyboardButton(
-    "Rate me ❤️", url="https://t.me/BotsArchive/1657")]]
+    "🙊OWNER OF BOT🙊", url="https://t.me/Call_me_futurepilot")]]
